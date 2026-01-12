ad4bcab511ac00847fe4671829221c94ba9a20e5
Unnati Rane <unnati.notes12@gmail.com>
2026-01-12 19:06:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5407415270-ad9b6",
+  "appId": "1:935806321564:web:e504f80a1bcfe3e7b23e59",
+  "apiKey": "AIzaSyBhs70_2sNC1ERI-fcK1-eLJblKZGufzc8",
+  "authDomain": "studio-5407415270-ad9b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "935806321564"
+};
